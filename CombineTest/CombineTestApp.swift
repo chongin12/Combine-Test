21d9dfa697a3d18f190f16d4b05d0ae9ead2249d
Jeong Chong In <chongin12@naver.com>
2024-06-08 17:06:30 +0900

    Initial Commit

---
+++ CombineTest/CombineTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineTestApp.swift
+//  CombineTest
+//
+//  Created by 정종인 on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CombineTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
